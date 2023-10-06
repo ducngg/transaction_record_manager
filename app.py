fb016f67be1b7bc104fc0dc2261844159fc4c81a
ducngg <ducng1311@gmail.com>
2023-10-06 04:36:29 -0400

    add CRUD functions

--- app.py
+++ app.py
@@ -1,16 +1,70 @@
-# Import libraries
+from flask import Flask, redirect, request, render_template, url_for
+app = Flask(__name__)
 
-# Instantiate Flask functionality
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
-# Sample data
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions=transactions)
 
-# Read operation
+@app.route("/add", methods=["GET", "POST"])
+def add_transaction():
+    if request.method == 'POST':
+        transaction = {
+            'id': len(transactions) + 1,
+            'date': request.form['date'],
+            'amount': float(request.form['amount'])
+        }
+        transactions.append(transaction)
 
-# Create operation
+        return redirect(url_for("get_transactions"))
+    return render_template("form.html")
 
-# Update operation
+@app.route("/edit/<int:transaction_id>", methods=["GET", "POST"])
+def edit_transaction(transaction_id):
+    if request.method == 'POST':
+        date = request.form['date']
+        amount = float(request.form['amount'])
 
-# Delete operation
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
 
-# Run the Flask app
+        return redirect(url_for("get_transactions"))
     
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            return render_template("edit.html", transaction=transaction)
+
+@app.route("/delete/<int:transaction_id>")
+def delete_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transactions.remove(transaction)
+            break
+
+    return redirect(url_for("get_transactions"))
+
+@app.route("/search", methods=["GET", "POST"])
+def search_transactions():
+    if request.method == 'POST':
+        min_amount = float(request.form['min_amount'])
+        max_amount = float(request.form['max_amount'])
+
+        filtered_transactions = []
+
+        for transaction in transactions:
+            if transaction['amount'] >= min_amount and transaction['amount'] <= max_amount: 
+                filtered_transactions.append(transaction)
+
+        return render_template("transactions.html", transactions=filtered_transactions)
+    return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
